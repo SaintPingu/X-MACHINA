1f089fc63c54e8f708a9ac7a6a93878ad17c0bfa
SaintPingu <45813433+SaintPingu@users.noreply.github.com>
2024-05-12 18:17:13 +0900

    change directional light options

--- Client/X-Engine/src/Light.cpp
+++ Client/X-Engine/src/Light.cpp
@@ -171,9 +171,9 @@ void Light::SetSunlight()
 	LightLoadInfo& light = mLoadLights->Lights[gkSunLightIdx];
 	light.Type           = static_cast<int>(LightType::Directional);
 	light.Ambient        = Vec4(0.1f, 0.1f, 0.1f, 1.f);
-	light.Diffuse        = Vec4(0.6f, 0.6f, 0.6f, 1.f);
+	light.Diffuse        = Vec4(0.3f, 0.3f, 0.3f, 1.f);
 	light.Specular       = Vec4(0.5f, 0.5f, 0.5f, 1.f);
-	light.Direction		 = Vec3(0.57735f, -0.57735f, 0.57735f);
+	light.Direction		 = Vec3(1, -2, 1);
 	light.IsEnable		 = true;
 }
 

dcfa74fc5c8f705cd24bac5a5d39659a22e012a9
SaintPingu <45813433+SaintPingu@users.noreply.github.com>
2024-08-07 21:35:18 +0900

    set weapon UI (all)

--- Client/X-MACHINA/src/WeaponUI.cpp
+++ Client/X-MACHINA/src/WeaponUI.cpp
@@ -16,7 +16,12 @@ WeaponUI::WeaponUI(const Vec2& position)
 void WeaponUI::SetWeapon(rsptr<Script_Weapon> weapon)
 {
 	static const std::unordered_map<WeaponName, std::string> kWeaponUIMap{
+		{ WeaponName::H_Lock, "WeaponUI_H_Lock"},
 		{ WeaponName::SkyLine, "WeaponUI_Skyline"},
+		{ WeaponName::DBMS, "WeaponUI_DBMS"},
+		{ WeaponName::Burnout, "WeaponUI_Burnout"},
+		{ WeaponName::PipeLine, "WeaponUI_PipeLine"},
+		{ WeaponName::MineLauncher, "WeaponUI_MineLauncher"},
 	};
 
 	static constexpr Vec2 kWeaponUIPosOffset{ -10, -20 };
@@ -33,7 +38,7 @@ void WeaponUI::SetWeapon(rsptr<Script_Weapon> weapon)
 
 	const std::string& weaponUIName = kWeaponUIMap.at(weaponName);
 	const Vec2 weaponUIPos = mPos + kWeaponUIPosOffset;
-	mWeaponUI = Canvas::I->CreateUI<UI>(1, weaponUIName, weaponUIPos);
+	mWeaponUI = Canvas::I->CreateUI<UI>(1, weaponUIName, weaponUIPos, Vec2(190, 90) * 0.9f);
 }
 
 void WeaponUI::Update()
